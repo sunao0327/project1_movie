ea9ea40c7b60665d9dcf33322271b15f1e0bd64a
kohiisunao <sunao@kohiisunao-no-MacBook-Air.local>
2020-05-29 18:03:48 +0900

    project1_movie

---
+++ data4.php
@@ -0,0 +1,27 @@
+<style>
+	.shadow1{
+		display: inline-block;
+  		border-radius: 5px;
+  		background-color: black;
+  		padding: 15px;
+  		text-align: center;
+  		color: white;
+  		width: 200px;
+  		font-size: 30px;
+  	}
+  	
+  	 body {
+		background-color: #DBDBDB;
+	}
+
+</style>
+
+
+
+
+<form action = data41.php method = "POST">
+	<input type = "submit" name = "kind1" value = "実話に基づいた感動映画" class = "shadow1">
+	</br>
+	</br>
+	<input type = "submit" name = "kind2" value = "家族を題材にした感動映画" class = "shadow1">
+</form>
