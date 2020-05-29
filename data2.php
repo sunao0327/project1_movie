51dcbead128f3a20ee9e8b4a758026c2f3d5ebc6
kohiisunao <sunao@kohiisunao-no-MacBook-Air.local>
2020-05-29 12:01:40 +0900

    project1_movie

---
+++ data2.php
@@ -0,0 +1,24 @@
+<style>
+	.shadow2{
+		display: inline-block;
+  		border-radius: 5px;
+  		background-color: black;
+  		padding: 15px;
+  		text-align: center;
+  		color: white;
+  		width: 200px;
+  		font-size: 30px;
+  	}
+  	
+  	 body {
+		background-color: #DBDBDB;
+	}
+</style>
+
+
+<form action = data21.php method = "POST">
+	<input type = "submit" name = "kind1" value = "アクション" class = "shadow2">
+	</br>
+	</br>
+	<input type = "submit" name = "kind2" value = "人間ドラマ" class = "shadow2">
+</form>
