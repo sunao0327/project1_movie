e6875bf3bd5ee567f9b6f92f7c4021e37db7e2eb
sunao <kohii0327@gmail.com>
2020-06-08 21:15:46 +0900

    change

--- data41.php
+++ data41.php
@@ -1,4 +1,6 @@
 <?php
+require_once ('data41_2.php');
+
 /*感動を選んだ場合*/
 	if (isset ($_POST['kind1'])) {
 		
@@ -6,18 +8,19 @@
 		echo '<br>' ;
 		echo '<br>' ;
 		
-		$array = array ("しあわせの隠れ場所", "最強のふたり", "グリーンブック", "ボヘミアンラプソディ") ;
+		$array = array ("しあわせの隠れ場所", "最強のふたり",  "ボヘミアンラプソディ") ;
 		$randkey = $array[array_rand( $array)] ;
 		
 		echo 'あなたへのおすすめは『 ' . $randkey . '』!' ; 
 		echo '<br>' ;
 		echo '<br>' ;
 		echo 'ちょっと違う・・・別の作品を表示する';
+		echo '『' . htmlspecialchars($row['title'],ENT_QUOTES,'UTF-8') . '』';
 		echo '<br>' ;
 		
 	}
-	if ( isset ($_POST['kind2'])){    /*サスペンスを選んだ場合*/
-		
+	if ( isset ($_POST['kind2'])){   
+	require_once ('data41_3.php');
 		echo '家族がテーマの感動する映画ですね！';
 		echo '<br>' ;
 		echo '<br>' ;
@@ -29,6 +32,7 @@
 		echo '<br>' ;
 		echo '<br>' ;
 		echo 'ちょっと違う・・・別の作品を表示する';
+		echo '『' . htmlspecialchars($row['title'],ENT_QUOTES,'UTF-8') . '』';
 		
 	}
 	
