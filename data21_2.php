9ae43eae36066d1c162a93310f70068dd0e0f95c
sunao <kohii0327@gmail.com>
2020-06-08 21:08:26 +0900

    エラー修正

--- data21_2.php
+++ data21_2.php
@@ -10,38 +10,36 @@ $mysqli = new mysqli($server, $userName, $password,$dbName);
  
 if ($mysqli -> connect_error){
 	echo $mysqli -> connect_error;
-	exit();21
+	exit(); 
 }else{
 	$mysqli->set_charset("utf-8");
 }
-
+//処理
 $sql = "SELECT movie_no, title FROM movie where genre = 'アクション' ORDER BY rand () LIMIT 1";
 
 $result = $mysqli -> query($sql);
 
-//クエリー失敗
 if(!$result) {
 	echo $mysqli->error;
 	exit();
 }
 
-//レコード件数
+
 $row_count = $result->num_rows;
 
-//連想配列で取得
+
 while($row = $result->fetch_array(MYSQLI_ASSOC)){
 	$rows[] = $row;
 }
 
-//結果セットを解放
 $result->free();
 
-// データベース切断
+
 $mysqli->close();
  
 ?>
 
-<!--ここからはコメントアウト（いらないやつだけど一応残してる！）
+<!--
 <!DOCTYPE html>
 <html>
 <head>
@@ -72,4 +70,4 @@ foreach($rows as $row){
 
 </body>
 </html>
- -->
+-->
