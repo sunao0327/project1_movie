7fef7b0afb4b8b08749cbdb87127d7bc4876ff40
sunao <kohii0327@gmail.com>
2020-06-08 21:06:36 +0900

    DB接続を追加

--- data21.php
+++ data21.php
@@ -1,36 +1,30 @@
 <?php
-require_once ('data21_2.php') ;
-?>
+require_once ('data21_2.php');
 
-
-
-<?php
 /*アクション大作を選んだ場合*/
 	if (isset ($_POST['kind1'])) {
 		
 		echo 'スカッとするアクション大作ですね！' ;
 		echo '<br>' ;
 		echo '<br>' ;
-		
-		/*
-		$array = array ("アベンジャーズ", "トランスフォーマー", "キングスマン", "マグニフィセントセブン") ;
-		$randkey = $array[array_rand( $array)] ;
-		*/
-		
-		/*echo 'あなたへのおすすめは『 ' . $randkey . '』!' ; */
 		echo '<br>' ;
 		echo ' あなたへのおすすめは' ;
 		echo '<br>' ;
+		echo '<br>' ;
 		/*dbからランダムで表示する*/
-		echo htmlspecialchars($row['title'],ENT_QUOTES,'UTF-8');
+		echo '『' . htmlspecialchars($row['title'],ENT_QUOTES,'UTF-8') . '』';
 		echo '<br>' ;
 		echo '<br>' ;
+		echo 'がおすすめです！';
+		echo '<br>' ;
+		/*
 		echo 'ちょっと違う・・・別の作品を表示する';
 		echo '<br>' ;
-		
+		*/
 	}
-	if ( isset ($_POST['kind2'])){    /*人間ドラマを選んだ場合*/	  
-	  
+	/*人間ドラマを選んだ場合*/
+	if ( isset ($_POST['kind2'])){    	  
+	  require_once ('data21_3.php');
 		echo 'スカッとする人間ドラマの映画ですね！';
 		echo '<br>' ;
 		echo '<br>' ;
@@ -41,8 +35,15 @@ require_once ('data21_2.php') ;
 		echo 'あなたへのおすすめは『 ' . $randkey . '』!' ; 
 		echo '<br>' ;
 		echo '<br>' ;
-		echo 'ちょっと違う・・・別の作品を表示する';
+		echo '<br>' ;
+		echo 'がおすすめです！';
+		echo '<br>' ;
 		
+		echo 'ちょっと違う・・・別の作品を表示する';
+		echo '<br>' ;
+		echo '『' . htmlspecialchars($row['title'],ENT_QUOTES,'UTF-8') . '』';
+
 	}
 	
-	?>
+?>
+	
