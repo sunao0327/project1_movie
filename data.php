c77c2604f9f3f0558467254af0abd188f42d05f6
sunao <kohii0327@gmail.com>
2020-06-08 21:04:35 +0900

    change

--- data.php
+++ data.php
@@ -1,8 +1,5 @@
- 
-
-
 <?php
-/*form1.phpで選択肢で得た情報で、ここでさらに選択肢を表示したいんですけど！！！*/
+/*form1.phpで選択肢で得た情報で、ここでさらに選択肢を表示*/
 
 
 if(isset($_POST["submit1"])){
@@ -12,7 +9,6 @@ if(isset($_POST["submit1"])){
     echo '<br>' ;
     echo '<br>' ;
     
-    /*ここにさらにジャンルを選ぶボタンを配置したいけど、formタグが置けない・・・php文で作れるか調べる！*/
     
     /*data2.phpでhtml形式でフォームタグ作ってそれを呼び出す*/
     
@@ -25,10 +21,8 @@ if(isset($_POST["submit2"])){
 		echo 'どんでん返しのある映画ですね！さらにジャンルを選んでください！' ;
 		echo '<br>' ;
     	echo '<br>' ;
-    	
-    /*ここにさらにジャンルを選ぶボタンを配置したいけど、formタグが置けない・・・php文で作れるか調べる！*/
     
-    /*data3.phpでhtml形式でフォームタグ作ってそれを呼び出す*/
+    /*data3.phpフォーム、css呼び出し*/
     require_once ('data3.php') ;
 }
 if(isset($_POST["submit3"])){
